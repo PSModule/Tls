80b0203f753aec6ba34381c41598fd913d4bd1ea
Marius Storhaug <marstor@hotmail.com>
2025-02-04 23:55:22 +0100

    Fix description syntax in TLS tests and improve formatting for clarity

--- tests/TLS.Tests.ps1
+++ tests/TLS.Tests.ps1
@@ -5,7 +5,7 @@
 [CmdletBinding()]
 param()
 
-Description 'TLS' {
+Describe 'TLS' {
     Context 'Get-TLSConfig' {
         It 'Should return the current TLS configuration by default' {
             $result = Get-TLSConfig
@@ -38,8 +38,8 @@ Description 'TLS' {
         It 'Should enable multiple TLS versions when specified' {
             Set-TLSConfig -Protocol Tls12, Tls13
             $result = [System.Net.ServicePointManager]::SecurityProtocol
-        ($result -band [System.Net.SecurityProtocolType]::Tls12) | Should -Be Tls12
-        ($result -band [System.Net.SecurityProtocolType]::Tls13) | Should -Be Tls13
+            $result -band [System.Net.SecurityProtocolType]::Tls12 | Should -Be Tls12
+            $result -band [System.Net.SecurityProtocolType]::Tls13 | Should -Be Tls13
         }
 
         It 'Should not remove existing TLS settings when adding new ones' {
