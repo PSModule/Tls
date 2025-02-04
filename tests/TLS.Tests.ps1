c02e2453c9f5491d01cd0aca16aeac95786a6cb3
Marius Storhaug <marstor@hotmail.com>
2025-02-04 23:33:20 +0100

    Add tests for Get-TLSConfig and Set-TLSConfig functions

--- tests/TLS.Tests.ps1
+++ tests/TLS.Tests.ps1
@@ -1,5 +1,45 @@
-﻿Describe 'Module' {
-    It 'Function: Get-PSModuleTest' {
-        Get-PSModuleTest -Name 'World' | Should -Be 'Hello, World!'
+﻿Describe 'Get-TLSConfig' {
+    It 'Should return the current TLS configuration by default' {
+        $result = Get-TLSConfig
+        $expectedTypes = [System.Net.SecurityProtocolType]
+        $result | Should -BeOfType $expectedTypes
+    }
+
+    It 'Should return available TLS versions when -ListAvailable is specified' {
+        $result = Get-TLSConfig -ListAvailable
+        $expectedValues = [enum]::GetValues([System.Net.SecurityProtocolType])
+        $result | Should -Be $expectedValues
+    }
+}
+
+Describe 'Set-TLSConfig' {
+    BeforeAll {
+        $originalTLS = [System.Net.ServicePointManager]::SecurityProtocol
+    }
+
+    AfterAll {
+        [System.Net.ServicePointManager]::SecurityProtocol = $originalTLS
+    }
+
+    It 'Should enable TLS 1.2 when specified' {
+        Set-TLSConfig -Protocol Tls12
+        $result = [System.Net.ServicePointManager]::SecurityProtocol
+        $result.HasFlag([System.Net.SecurityProtocolType]::Tls12) | Should -BeTrue
+    }
+
+    It 'Should enable multiple TLS versions when specified' {
+        Set-TLSConfig -Protocol Tls12, Tls13
+        $result = [System.Net.ServicePointManager]::SecurityProtocol
+                ($result -band [System.Net.SecurityProtocolType]::Tls12) | Should -Be [System.Net.SecurityProtocolType]::Tls12
+                ($result -band [System.Net.SecurityProtocolType]::Tls13) | Should -Be [System.Net.SecurityProtocolType]::Tls13
+    }
+
+    It 'Should not remove existing TLS settings when adding new ones' {
+        Set-TLSConfig -Protocol Tls11
+        $resultBefore = [System.Net.ServicePointManager]::SecurityProtocol
+        Set-TLSConfig -Protocol Tls12
+        $resultAfter = [System.Net.ServicePointManager]::SecurityProtocol
+        $resultAfter.HasFlag([System.Net.SecurityProtocolType]::Tls11) | Should -BeTrue
+        $resultAfter.HasFlag([System.Net.SecurityProtocolType]::Tls12) | Should -BeTrue
     }
 }
